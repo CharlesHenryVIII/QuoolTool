cc18dd08015c6aea08174a3503ea463c20e18137
CHoman <Tonykart84@gmail.com>
2026-03-02 10:19:12 -0700

    Fixed scripts

--- src/Tools.h
+++ src/Tools.h
@@ -1,4 +1,10 @@
 #pragma once
 #include "Settings.h"
 
+enum ScriptInfoFlags : u32 {
+    ScriptInfoFlags_None            = 0,
+    ScriptInfoFlags_Enabled         = BIT(0),
+    ScriptInfoFlags_ManualOutput    = BIT(1),
+};
+
 void ToolsImGui(ToolsData& td);
