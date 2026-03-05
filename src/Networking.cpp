09f9f7f83e2dff2409ba15f7014c902b87e4b5eb
CHoman <Tonykart84@gmail.com>
2026-03-05 11:27:03 -0700

    Removed asio

--- src/Networking.cpp
+++ src/Networking.cpp
@@ -1,4 +1,3 @@
-#if 1
 #include "curl/curl.h"
 
 #include "Networking.h"
@@ -168,59 +167,3 @@ void NetworkShutdown()
 {
 
 }
-
-
-#else
-#include "asio.hpp"
-#include "asio/ts/buffer.hpp"
-#include "asio/ts/internet.hpp"
-
-#include "Networking.h"
-#include "WinInterop.h"
-
-#include <vector>
-
-
-struct NetworkInfo {
-    asio::io_context context;
-};
-NetworkInfo s_network;
-
-void NetworkingInit()
-{
-    asio::error_code ec;
-    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
-    asio::ip::tcp::socket socket(s_network.context);
-    socket.connect(endpoint, ec);
-    if (!ec)
-    {
-        DebugPrint("Connected!");
-    }
-    else
-    {
-        DebugPrint("Connected!");
-    }
-
-    if (socket.is_open())
-    {
-        std::string request = "GET https://api.github.com/repos/CharlesHenryVIII/UATHelper/releases/latest\r\n\r\n";
-            //"Get /index.html HTTP/1.1\r\n"
-            //"Host: example.com\r\n"
-            //"Connection: close\r\n\r\n";
-        socket.write_some(asio::buffer(request.data(), request.size()), ec);
-
-        socket.wait(socket.wait_read);
-
-        size_t bytes = socket.available();
-        DebugPrint("Bytes Available: %i", bytes);
-
-        if (bytes > 0)
-        {
-            std::vector<char> buffer(bytes);
-            socket.read_some(asio::buffer(buffer.data(), buffer.size()), ec);
-        }
-    }
-
-    i32 test = 1;
-}
-#endif
