bcc4873bae9e347c4cfc937ce6adf2f4962f6adb
CHoman <Tonykart84@gmail.com>
2026-03-20 08:27:52 -0700

    Updated version

--- src/Version.cpp
+++ src/Version.cpp
@@ -1,7 +1,7 @@
 #include "Version.h"
 #include "WinInterop.h"
 
-Version g_version = { .major = 1, .minor = 3 };
+Version g_version = { .major = 1, .minor = 4 };
 
 std::string Version::AsTagString() const
 {
