c151e11deda0b11fe91dc846d6d13dc2a1432d82
CHoman <Tonykart84@gmail.com>
2026-03-27 13:12:26 -0700

    Cleaned up the batch script

--- WindowsXP.bat
+++ WindowsXP.bat
@@ -9,6 +9,9 @@ set SYSNAME=%COMPUTERNAME%
 set OUTDIR=%CD%\%SYSNAME%
 mkdir "%OUTDIR%" 2>nul
 
+set "TOTAL_STEPS=12"
+set "STEP_COUNT=0"
+
 echo Collecting data into %OUTDIR%
 echo.
 
@@ -26,16 +29,13 @@ set "SYS_VER=1"
 >> "%OUTDIR%\quooltoolinfo.xml" echo   ^<Version^>%SYS_VER%^</Version^>
 >> "%OUTDIR%\quooltoolinfo.xml" echo ^</SystemIdentity^>
 
-:: -----------------------------
-:: NETSTAT
-:: -----------------------------
-echo 01/11 Netstat...
+call :LogStep "Netstat..."
 netstat -ano > "%OUTDIR%\netstat.txt"
 
 :: -----------------------------
 :: Installed Programs
 :: -----------------------------
-echo 02/11 Gathering Installed Programs (This may take a moment)...
+call :LogStep "Gathering Installed Programs (This may take a while)..."
 ::reg query "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall" /s > "%OUTDIR%\programs.txt"
 
 REM Initialize the XML file with standard headers
@@ -86,104 +86,37 @@ REM Close the main XML tag
 
 
 
-:: -----------------------------
-:: Processor Info
-:: -----------------------------
-echo 03/11 CPU Info...
+call :LogStep "CPU Info..."
 wmic cpu get Name,NumberOfCores,NumberOfLogicalProcessors,MaxClockSpeed /format:rawxml > "%OUTDIR%\processor.xml"
 
-:: -----------------------------
-:: System Info (OS + Computer + BIOS + Timezone)
-:: -----------------------------
-echo 04/11 System Info...
+call :LogStep "System Info..."
 wmic computersystem get Name,Manufacturer,Model,SystemType,UserName,TotalPhysicalMemory /format:rawxml > "%OUTDIR%\computersystem.xml"
 
-echo 05/11 OS Info...
+call :LogStep "OS Info..."
 wmic os get /format:rawxml > "%OUTDIR%\os.xml"
 
-echo 06/11 BIOS Info...
+call :LogStep "BIOS Info..."
 wmic bios get Name,Manufacturer,SMBIOSBIOSVersion,SerialNumber,ReleaseDate /format:rawxml > "%OUTDIR%\bios.xml"
 
-echo 07/11 Timezone Info...
+call :LogStep "Timezone Info..."
 wmic timezone get Caption /format:rawxml > "%OUTDIR%\timezone.xml"
 
-:: -----------------------------
-:: GPU Info
-:: -----------------------------
-echo 08/11 GPU Info...
+call :LogStep "GPU Info..."
 wmic path Win32_VideoController get Name,DriverVersion,AdapterRAM,VideoProcessor,CurrentHorizontalResolution,CurrentVerticalResolution /format:rawxml > "%OUTDIR%\gpu.xml"
 
-:: -----------------------------
-:: Disk Info (Logical)
-:: -----------------------------
-echo 09/11 Disk Info (Logical Drives)...
-::wmic logicaldisk where "DriveType=3" get DeviceID,FileSystem,Size,FreeSpace,VolumeName /format:rawxml > "%OUTDIR%\logical_disks.xml"
+call :LogStep "Logical Disk Info..."
 wmic logicaldisk get /format:rawxml > "%OUTDIR%\logical_disks.xml"
 
-:: -----------------------------
-:: Physical Disk Info
-:: -----------------------------
-echo 10/11 Physical Disk Info...
-::wmic diskdrive get Model,InterfaceType,Size,SerialNumber /format:csv > "%OUTDIR%\physical_disks.csv"
+call :LogStep "Physical Disk Info..."
 wmic diskdrive get Model,MediaType,Name,Description,Size,Manufacturer,Partitions,Status,InterfaceType,FirmwareRevision,DeviceID /format:rawxml > "%OUTDIR%\physical_disks.xml"
 
-REM :: Define a temporary VBScript file
-REM set "VBSFILE=%TEMP%\get_disks_safe.vbs"
-REM 
-REM :: Write the Byte Formatting Helper Function
-REM > "%VBSFILE%" echo Function F(b)
-REM >> "%VBSFILE%" echo If IsNull(b) Or b = "" Then F = "0 B" : Exit Function
-REM >> "%VBSFILE%" echo Dim v : v = CDbl(b)
-REM >> "%VBSFILE%" echo If v ^>= 1099511627776 Then
-REM >> "%VBSFILE%" echo     F = Round(v/1099511627776, 2) ^& " TB"
-REM >> "%VBSFILE%" echo ElseIf v ^>= 1073741824 Then
-REM >> "%VBSFILE%" echo     F = Round(v/1073741824, 2) ^& " GB"
-REM >> "%VBSFILE%" echo ElseIf v ^>= 1048576 Then
-REM >> "%VBSFILE%" echo     F = Round(v/1048576, 2) ^& " MB"
-REM >> "%VBSFILE%" echo Else
-REM >> "%VBSFILE%" echo     F = v ^& " B"
-REM >> "%VBSFILE%" echo End If
-REM >> "%VBSFILE%" echo End Function
-REM 
-REM >> "%VBSFILE%" echo On Error Resume Next
-REM >> "%VBSFILE%" echo Set wmi = GetObject("winmgmts:{impersonationLevel=impersonate}^!\\.\root\cimv2")
-REM 
-REM :: Loop 1: Physical Hardware
-REM >> "%VBSFILE%" echo WScript.Echo "--- PHYSICAL DISKS ---"
-REM >> "%VBSFILE%" echo WScript.Echo "DeviceID,Model,Interface,Size"
-REM >> "%VBSFILE%" echo For Each d In wmi.ExecQuery("Select * From Win32_DiskDrive")
-REM >> "%VBSFILE%" echo   modName = Replace(d.Model ^& "", ",", " ")
-REM >> "%VBSFILE%" echo   WScript.Echo d.DeviceID ^& "," ^& modName ^& "," ^& d.InterfaceType ^& "," ^& F(d.Size)
-REM >> "%VBSFILE%" echo Next
-REM 
-REM :: Loop 2: Windows Drive Letters
-REM >> "%VBSFILE%" echo WScript.Echo ""
-REM >> "%VBSFILE%" echo WScript.Echo "--- LOGICAL DRIVES ---"
-REM >> "%VBSFILE%" echo WScript.Echo "DriveLetter,VolumeName,Size,FreeSpace"
-REM >> "%VBSFILE%" echo For Each l In wmi.ExecQuery("Select * From Win32_LogicalDisk Where DriveType=3")
-REM >> "%VBSFILE%" echo   volName = Replace(l.VolumeName ^& "", ",", " ")
-REM >> "%VBSFILE%" echo   WScript.Echo l.DeviceID ^& "," ^& volName ^& "," ^& F(l.Size) ^& "," ^& F(l.FreeSpace)
-REM >> "%VBSFILE%" echo Next
-REM 
-REM :: Execute the script and save the output
-REM cscript //nologo "%VBSFILE%" > "%OUTDIR%\disks.csv"
-REM 
-REM :: Clean up the temp file
-REM del "%VBSFILE%"
-
-:: -----------------------------
-:: IP Config
-:: -----------------------------
-echo 11/12 Network Info...
-REM ipconfig /all > "%OUTDIR%\ipconfig.txt"
-REM wmic nicconfig where "IPEnabled='TRUE'" get Description,MACAddress,IPAddress,IPSubnet,DefaultIPGateway,DHCPServer,DNSServerSearchOrder /format:rawxml > "%OUTDIR%\network.xml"
-REM wmic nicconfig get * /format:rawxml > "%OUTDIR%\network.xml"
+call :LogStep "Network Info..."
 wmic nicconfig get Description,IPEnabled,SettingID,DHCPEnabled,MACAddress /format:rawxml > "%OUTDIR%\networks.xml"
 
 :: -----------------------------
-:: Saved IP Configurations (Pure Batch XML)
+:: Network Adapters
 :: -----------------------------
-echo 12/12 Extracting Complete Network Profile...
+call :LogStep "Extracting Network Adapters..."
 
 :: 1. Write the standard XML header
 > "%OUTDIR%\network_settings.xml" echo ^<?xml version="1.0" encoding="UTF-8"?^>
@@ -263,7 +196,25 @@ FOR /F "delims=" %%A IN ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpi
 )
 
 >> "%OUTDIR%\network_settings.xml" echo ^</NetworkInterfaces^>
-echo Done! Network data saved to %OUTDIR%\network_settings.xml
+
+
+REM -----------------------------
+REM Helper Functions
+REM -----------------------------
+:LogStep
+set /a STEP_COUNT+=1
+
+if !STEP_COUNT! LSS 10 (
+    set "PAD=0!STEP_COUNT!"
+) else (
+    set "PAD=!STEP_COUNT!"
+)
+
+REM Slice the string to only keep the last 2 characters (!PAD:~-2!)
+REM This turns "01" into "01", and "010" into "10"
+echo !PAD:~-2!/%TOTAL_STEPS% %~1
+
+goto :EOF
 
 
 
