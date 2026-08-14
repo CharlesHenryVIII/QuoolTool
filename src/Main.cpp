5b09868ffe6d03c8b32f2c0804d4e1a612fdff8f
CHoman <Tonykart84@gmail.com>
2026-08-14 05:20:14 -0700

    Added default theme

--- src/Main.cpp
+++ src/Main.cpp
@@ -111,7 +111,7 @@ i32 SysMain(i32 argc, char** argv)
     ImguiInit();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     ImGuiStyle& style = ImGui::GetStyle();
-    ThemesInit(g_theme_settings.color, g_theme_settings.style);
+    ThemesInit(g_theme_settings.color, g_theme_settings.style, ThemeColor_Quantum, ThemeStyle_SimpleRounding);
 
 
 
